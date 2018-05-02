d4540876a7d38157e9af45f468d4a9a87fb48e72
Leonard Andrew Spencer <thrawn369@gmail.com>
2018-05-02 22:01:57 +0930

    Starting work on CSVLoader class to parse csv files

---
+++ Dialogue System Project/Assets/Dialogue System/Scripts/Localization Loaders/CSVLoader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dialogue
+{
+    [CreateAssetMenu(menuName = "Dialogue/Localization/CSV Loader")]
+    public class CSVLoader : LocalizationLoader
+    {
+        [SerializeField]
+        TextAsset localizationData;
+
+        public override Dictionary<string, string> LoadLanguage(string locale)
+        {
+            //TODO just use standard csv parser
+            
+            string[] fileLines = localizationData.text.Split(new string[] { "\n\r" },StringSplitOptions.RemoveEmptyEntries);
+            string[] header = fileLines[0].Split(',');  //TODO Fix this: breaks because quotes aren't escaped
+            int localeIndex = Array.IndexOf(header, locale);
+            if(localeIndex >= 0)
+            {
+                Dictionary<string, string> loadedLines = new Dictionary<string, string>();
+                for (int i = 1; i < fileLines.Length; ++i)
+                {
+                    string[] values = fileLines[i].Split(',');
+                    try {
+                        string key = values[0];
+                        string translation = values[localeIndex];
+                        loadedLines.Add(key, translation);
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        Debug.LogWarning("Translation not found on line " + i.ToString());
+                    }
+                }
+                return loadedLines;
+            }
+            else
+            {
+                // Locale not found
+                return null;
+            }
+        }
+    }
+}
---
+++ Dialogue System Project/Assets/Dialogue System/Scripts/Localization Loaders/LocalizationLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dialogue {
+    public class LocalizationLoader : ScriptableObject
+    {
+        public virtual Dictionary<string, string> LoadLanguage(string locale)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
